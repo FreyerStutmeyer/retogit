8a0de7391344df025f65c0a78cece509cbbd4b4a
Fernando Berenguer <168065872+FreyerStutmeyer@users.noreply.github.com>
2024-05-01 17:45:03 +0200

    Add files via upload
    Subiendo la carpeta src de un proyecto java para el reto de entornos

---
+++ src/Hola.java
@@ -0,0 +1,7 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Hola {
+    public static void main(String[] args) {
+        System.out.println("Esto es una prueba para el repositorio de entornos");
+    }
+}
