86e6fd11dfbf248ae7b842e7325f33bb91752849
Fernando Berenguer <168065872+FreyerStutmeyer@users.noreply.github.com>
2024-05-01 18:23:01 +0200

    Add files via upload
    Edición clase - NuevaClase.java

--- src/NuevaClase.java
+++ src/NuevaClase.java
@@ -3,6 +3,6 @@ public class NuevaClase {
         System.out.println("Clase para actualizar");
 
 
-        System.out.println("Modificacion 3 a comprobar");
+        System.out.println("Editando la clase");
     }
 }
